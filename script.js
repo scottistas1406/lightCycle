28d894633561898ae285fecc13e648be1144ccf1
Scott Istas <120509564+scottistas1406@users.noreply.github.com>
2023-03-19 19:13:05 -0500

    lightcycle3 lots of changes

--- script.js
+++ script.js
@@ -6,36 +6,87 @@ let bikeX = 0;
 let bikeY = 0;
 let direction = "right";
 let gameOver = 0;
+let gameOverCrash = 0;
+let cycleSpeed = 50;
+
+// function drawGrid()
+// for(let i=0; i < canvas.width; i++){
+//    for( let k=0; k<canvas.height; k++)
+
+
+// }
 
 
 
 //console.log(gameOver);
 //runGame();
+function restartGame() {
+    location.reload();
+}
 function startGame() {
     let selectColorElement = document.getElementById("cycleColor")
     let selectColor = selectColorElement.value;
     teamColor = selectColor;
-    console.log(teamColor)
-    let gameInterval = setInterval(updateGame, 100);
+    //console.log(teamColor)
+    gameInterval = setInterval(updateGame, cycleSpeed);
+
+
+
+
 }
+
 //console.log(`hello`);
 let lightCycle = [
     //front of cycle
-    { x: 5, y: 0, velocity: 5, size: 15 }, //x is the position on the x and y axis, velocity controls the number of spaces the light cycle will travel...pixels? the smaller the number the slower it will appear to move
+    { x: 100, y: 100, velocity: 5, size: 10 }, //x is the position on the x and y axis, velocity controls the number of spaces the light cycle will travel...pixels? the smaller the number the slower it will appear to move
     //middle
-    { x: 10, y: 0, velocity: 5, size: 15, },
+    { x: -10, y: 0, velocity: 5, size: 5, },
     //back middle
-    { x: 15, y: 0, velocity: 5, size: 15, },
-    { x: 20, y: 0, velocity: 5, size: 15, },
-    { x: 25, y: 0, velocity: 5, size: 15, }
+    { x: -15, y: 0, velocity: 5, size: 5, },
+    { x: -20, y: 0, velocity: 5, size: 5, },
+    { x: -25, y: 0, velocity: 5, size: 5, },
+    { x: -30, y: 0, velocity: 5, size: 5, },
+    { x: -35, y: 0, velocity: 5, size: 5, },
+    { x: -20, y: 0, velocity: 5, size: 5, },
+    { x: - 25, y: 0, velocity: 5, size: 5, },
+    { x: -30, y: 0, velocity: 5, size: 5, },
+    { x: -20, y: 0, velocity: 5, size: 5, },
+    { x: -25, y: 0, velocity: 5, size: 5, },
+    { x: -30, y: 0, velocity: 5, size: 5, },
+    { x: - 20, y: 0, velocity: 5, size: 5, },
+    { x: -25, y: 0, velocity: 5, size: 5, },
+    { x: -30, y: 0, velocity: 5, size: 5, },
+    { x: -20, y: 0, velocity: 5, size: 5, },
+    { x: -25, y: 0, velocity: 5, size: 5, },
+    { x: -30, y: 0, velocity: 5, size: 5, },
+
 
 
 
 ];//put lightcycle in array...??
 
 
+function checkCrash() {
+    for (let i = 5; i < lightCycle.length; i++) {
+        //if the start of the bike 
+        //console.log(`firstvalue${lightCycle[0].x}`)
+        //console.log(`secondvalue${lightCycle[i].x}`);
+        //console.log(`lightCycleSize${lightCycle[0].size}`);
 
+        if (lightCycle[0].x >= lightCycle[i].x && lightCycle[0].x <= lightCycle[i].x + lightCycle[0].size) {
+            //if the head y value is between.
+            // console.log(`calc${lightCycle[i].x + lightCycle[0].size}`);
+            //  console.log(`first condition met`);
 
+            console.log("true first criteria")
+            if (lightCycle[0].y >= lightCycle[i].y && lightCycle[0].y <= lightCycle[i].y + lightCycle[i].size) {
+                console.log("crash")
+                alert("Program wall, crash");
+                gameOverCrash = 1;
+            }
+        }
+    }
+}
 
 
 function drawBike() {
@@ -55,25 +106,27 @@ function drawBike() {
 
 //function will run the game call all functions from here.
 function updateGame() {
-    if (gameOver === 0) {
+    if (gameOver === 0 && gameOverCrash === 0) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         updateBike(direction);
         drawBike();
+        checkCrash();
         checkOOB();
+        lightCycle.push({ x: -30, y: 0, velocity: 5, size: 5, })
         //console.log(gameOver);
         // runGame();
     }
     else {
-        stopGame()
+        clearInterval(gameInterval);
 
     }
 }
 
 function updateBike(axis) {
-    console.log(`axis ${axis}`)
-    console.log(lightCycle.length);
+    //  console.log(`axis ${axis}`)
+    // console.log(lightCycle.length);
     for (let i = lightCycle.length - 1; i > 0; i--) { //set to last position of array
-        console.log(`madeit ${lightCycle.length}`)
+        //console.log(`madeit ${lightCycle.length}`)
         lightCycle[i].x = lightCycle[i - 1].x;
         lightCycle[i].y = lightCycle[i - 1].y;
     }
@@ -95,10 +148,12 @@ function updateBike(axis) {
 
 }
 function checkOOB() {
+
     if (lightCycle[0].x < 0 || lightCycle[0].x >= canvas.width || lightCycle[0].y < 0 || lightCycle[0].y > canvas.height) {
         alert("Game Over");
         gameOver = 1;
         clearInterval(gameInterval);
+
 
     }
     else {
